25dde776e815f60a506101659711f22c737ff8a6
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2014-10-16 15:55:51 -0400

    Add rpc versions of transfer limit
    Add backend calls for `riak-admin transfers limit` that use rpc like the
    old version of `riak-admin transfer-limit`. The versions intended to be
    used without a flag use cluster metadata for efficiency.

--- src/riak_core_console.erl
+++ src/riak_core_console.erl
@@ -32,7 +32,8 @@
 	 stat_show/1, stat_info/1, stat_enable/1, stat_disable/1, stat_reset/1]).
 
 %% New API to be used by riak-admin transfers <X>
--export([transfers_limit/0, transfers_limit/1]).
+-export([transfers_limit/0, transfers_limit/1, rpc_transfers_limit/0,
+	rpc_transfers_limit/1]).
 
 %% @doc The following functions in this section are new commands intended to be
 %% visible in riak-admin transfer X commands. They are detailed in this RFC:
@@ -46,6 +47,14 @@ transfers_limit() ->
 
 transfers_limit(Node) ->
     Status = riak_core_status:transfer_limit(Node),
+    print_transfers_limit(Status).
+
+rpc_transfers_limit() ->
+    Status = riak_core_status:rpc_transfer_limit(),
+    print_transfers_limit(Status).
+
+rpc_transfers_limit(Node) ->
+    Status = riak_core_status:rpc_transfer_limit(Node),
     print_transfers_limit(Status).
 
 print_transfers_limit(Status) ->
